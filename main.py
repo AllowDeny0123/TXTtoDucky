165b24b7dd356019f2823a7f095df47d4f28ef5a
Anton <shabanov.anton.2012@gmail.com>
2023-03-30 16:51:51 +0500

    fixed typo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
         print('Prepairing for work...')
         a = DuckyConverter(
             sys.argv[1],
-            int(sys.argv[2]) if len(sys.argv) >= 3 else DuckyConverter.DEFAULTDELAY,
+            int(sys.argv[2]) if len(sys.argv) >= 3 else DuckyConverter.DEFAULT_DELAY,
             sys.argv[3] if len(sys.argv) >=4 else DuckyConverter.DEFAULT_OUT_NAME
             )
         a.ComposeFile()
